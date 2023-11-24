cbe81d959e91bc5c7713472df0f5fad4ad6e45e4
Claudio Perez <claudokid@gmail.com>
2023-11-23 21:17:57 -0500

    improve code challenge_03

--- CHALLENGE_03.js
+++ CHALLENGE_03.js
@@ -8,22 +8,21 @@ fs.readFile('files\\encryption_policies.txt', 'utf-8', (err, data) => {
   let listPwd = data.split('\r\n')
 
   listPwd.forEach(element => {
-    let initialArr = element.split(':')
-    let len = initialArr[1].trim().length
-    let stringToEvaluate = initialArr[1].trim()
-    let rulesArr = initialArr[0].split(' ')
-    let letterToEvaluate = rulesArr[1]
-    let numberOfOcurrences = rulesArr[0].split('-')[0]
-    let totalLength = parseInt(rulesArr[0].split('-')[1])
+    const [range, letter, password] = element
+      .replace(new RegExp(':', 'g'), '')
+      .split(' ')
+    const [min, max] = range.split('-')
 
     if (
-      (stringToEvaluate.match(new RegExp(letterToEvaluate, 'g')) || [])
-        .length != numberOfOcurrences ||
-      totalLength != len
+      (password.match(new RegExp(letter, 'g')) || []).length != min ||
+      max != password.trim().length
     ) {
-      listInvalidPwd.push(stringToEvaluate)
+      listInvalidPwd.push(password.trim())
     }
   })
-  console.log(listInvalidPwd[41])
-  console.log(listInvalidPwd[12])
+
+  if (listInvalidPwd.length > 41) {
+    console.log(listInvalidPwd[41])
+    console.log(listInvalidPwd[12])
+  }
 })
