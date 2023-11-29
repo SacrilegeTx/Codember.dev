504e7119eb36eb0d1e872b0db159f3d71e8a691c
Claudio Perez <claudokid@gmail.com>
2023-11-28 22:04:54 -0500

    Challenge 04 solved

---
+++ CHALLENGE_04.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+
+const findDuplicates = arr =>
+  Array.from(new Set(arr.filter((value, idx, ar) => idx !== ar.indexOf(value))))
+
+const isSameChecksum = (newChecksum, checksum) => checksum === newChecksum
+
+let realFiles = []
+
+fs.readFile('files\\files_quarantine.txt', 'utf-8', (err, data) => {
+  if (err) throw err
+
+  let allFiles = data.split('\r\n') //.slice(0, 9)
+  
+  allFiles.forEach(element => {
+    const [filename, checksum] = element.split('-')
+
+    let duplicated = findDuplicates(filename.split('')).join('')
+
+    if (duplicated) {      
+      let newChecksum = filename.replace(new RegExp(duplicated, 'g'), '')      
+      if (isSameChecksum(newChecksum, checksum)) realFiles.push(checksum)
+    } else {
+      if (isSameChecksum(filename, checksum)) realFiles.push(checksum)
+    }
+  })
+
+  console.log(realFiles[32])
+})
