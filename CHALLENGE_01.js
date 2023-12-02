f28d6b3b6f6322da840338b9661e7a10a5746ef2
Claudio Perez <claudokid@gmail.com>
2023-12-01 23:49:42 -0500

    use fs promises

--- CHALLENGE_01.js
+++ CHALLENGE_01.js
@@ -1,13 +1,11 @@
-const fs = require('fs')
+import fs from 'node:fs/promises'
 
 const count = arr => arr.reduce((ac, a) => ((ac[a] = ac[a] + 1 || 1), ac), {})
 
-fs.readFile('files\\message_01.txt', 'utf-8', (err, data) => {
-  if (err) throw err
+const file = await fs
+  .readFile('files\\message_01.txt', 'utf-8')
+  .catch(err => console.error('Error leyendo el archivo:', err.message))
 
-  let listAnimals = data.split(' ')
+let txt = (Object.entries(count(file.split(' '))).join('')).replace(new RegExp(',', 'g'), '')
 
-  let finalString = Object.entries(count(listAnimals)).join('')
-
-  console.log(finalString.replace(new RegExp(',', 'g'), ''))
-})
+console.log({ txt })
